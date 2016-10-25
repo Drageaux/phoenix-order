24499e620dbe740538b7d04f20dcada66b49e7a4
David Thong Nguyen <thnguyen2808@gmail.com>
2016-10-25 13:51:18 -0400

    Setting up frontend

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express"),
+    app = express(),
+    path = require("path"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/phoenix-order");
+
+// Import routers
+var playerRouter = require("./server/routers/playerRouter");
+// Settings
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+
+
+// Resource loading
+app.use("/node_modules", express.static(__dirname + "../node_modules"));
+app.use("/", express.static(__dirname + "/dist"));
+
+// API routers
+app.use("/player", playerRouter);
+
+// Serve HTML files
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+// Run app
+app.listen(process.env.PORT || '3000', function () {
+    console.log("I'm listening on PORT: " + (process.env.PORT || "3000"));
+});
